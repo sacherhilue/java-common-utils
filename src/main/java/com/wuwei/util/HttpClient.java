24d7c6d9f065fabfbcf88cd148683db4b187ea4a
sacherhilue <zhongshanyunwo@126.com>
2021-04-30 12:39:33 +0800

    更新 HttpClient.java

--- src/main/java/com/wuwei/util/HttpClient.java
+++ src/main/java/com/wuwei/util/HttpClient.java
@@ -5,6 +5,7 @@ import java.nio.charset.Charset;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.HttpClientUtils;
@@ -20,6 +21,7 @@ import org.apache.http.util.EntityUtils;
  * @date 2018/10/11 10:40
  */
 public class HttpClient {
+
     /**
      * 将文件提交至文件服务器
      *
@@ -33,12 +35,21 @@ public class HttpClient {
         String result = null;
         try {
             HttpPost httpPost = new HttpPost(url);
+            //超时设置
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setConnectTimeout(5000)//设置连接超时时间，单位毫秒
+                    .setConnectionRequestTimeout(3000)//设置从连接池获取Connection超时时间，单位毫秒
+                    .setSocketTimeout(5000)//请求获取数据的超时时间(即响应时间)，单位毫秒
+                    .build();
+            httpPost.setConfig(requestConfig);
+            //参数设置
             HttpEntity httpEntity = MultipartEntityBuilder.create()
                     .setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
                     .setCharset(Charset.forName("UTF-8"))
                     .addBinaryBody("file", file)
                     .build();
             httpPost.setEntity(httpEntity);
+            //执行请求
             response = httpclient.execute(httpPost);
             int statusCode = response.getStatusLine().getStatusCode();
             if (statusCode == HttpStatus.SC_OK) {
